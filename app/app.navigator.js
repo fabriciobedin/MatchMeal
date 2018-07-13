802a13255cfe6a2d522b80c77fb3cc0f02bee96b
Fabricio Bedin <fabricio-bedin@hotmail.com>
2018-07-12 23:52:58 -0300

    autentication with firebase working

--- app/app.navigator.js
+++ app/app.navigator.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { DrawerNavigator } from 'react-navigation';
+import { DrawerNavigator, StackNavigator } from 'react-navigation';
 import SplashScreen from './screens/splash.screen';
 import LoginScreen from './screens/login.screen';
+import MatchScreen from './screens/match.screen';
 const Splash = {
   screen: SplashScreen,
   navigationOptions: {
@@ -14,12 +15,24 @@ const Login = {
     header: null
   }
 }
+const Match = {
+  screen: MatchScreen,
+  navigationOptions: {
+    headerMode: 'screen',
+    headerTitle: 'Matches',
+    drawerLabel: 'Matches'
+  }
+}
+const MatchStack = StackNavigator({
+  Match: Match
+},{})
 const RouteConfig = {
   initialRoute: 'Splash'
 }
 const AppNavigator = DrawerNavigator({
   Splash: Splash,
-  Login: Login
+  Login: Login,
+  Match: {screen: MatchStack}
 },RouteConfig)
 
 export default AppNavigator;
--- app/stores/config.store.js
+++ app/stores/config.store.js
@@ -1,16 +1,29 @@
-export default class ConfigStore {
-  constructor() {
-    this.splashTime = 1000
-    this.splashImg = require('../../images/splash.jpg')
-    this.loginBG = require('../../images/login.jpg')
+import firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyDFqkVF9xpLxFHcjGAtldQs3qofdGoUSwA",
+    authDomain: "mathmeal.firebaseapp.com",
+    databaseURL: "https://mathmeal.firebaseio.com",
+    projectId: "mathmeal",
+    storageBucket: "mathmeal.appspot.com",
+    messagingSenderId: "57841852836"
+  };
+
+
+  export default class ConfigStore {
+    constructor() {
+      firebase.initializeApp(config)
+      this.splashTime = 1000
+      this.splashImg = require('../../images/splash.jpg')
+      this.loginBG = require('../../images/login.jpg')
+    }
+    get SplashImg() {
+      return this.splashImg
+    }
+    get SplashTime() {
+      return this.splashTime
+    }
+    get LoginBG() {
+      return this.loginBG
+    }
   }
-  get SplashImg() {
-    return this.splashImg
-  }
-  get SplashTime() {
-    return this.splashTime
-  }
-  get LoginBG() {
-    return this.loginBG
-  }
-}
