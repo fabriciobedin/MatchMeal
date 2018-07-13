c0875e539da1d1e967f83f0622f55f3fc32a6111
Fabricio Bedin <fabricio-bedin@hotmail.com>
2018-07-12 23:33:36 -0300

    screen login ok

--- app/screens/login.screen.js
+++ app/screens/login.screen.js
@@ -5,7 +5,9 @@ import {
 } from 'native-base';
 import {
   View,
-  Image
+  Image,
+  StyleSheet,
+  Dimensions
 } from 'react-native';
 import { inject } from 'mobx-react';
 
@@ -20,10 +22,10 @@ export default class LoginScreen extends Component {
     const {stores} = this.props
     return (
       <Container>
-        <View>
+        <View style={styles.container}>
           <Content scrollEnabled={false}>
-            <Image source={stores.config.LoginBG}>
-              <View>
+            <Image style={styles.loginBackground} source={stores.config.LoginBG}>
+              <View style={styles.loginForeground}>
                 <Login {...this.props}/>
               </View>
             </Image>
@@ -33,3 +35,26 @@ export default class LoginScreen extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  loginBackground: {
+    flex: 1,
+    width: null,
+    height: null
+  },
+  loginForeground: {
+    flex:1,
+    marginTop: Dimensions.get('window').height/1.75,
+    paddingTop: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 90,
+    bottom: 0
+  }
+})
